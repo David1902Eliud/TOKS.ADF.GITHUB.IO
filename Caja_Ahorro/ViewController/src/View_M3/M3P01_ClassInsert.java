ad029d0c083c04a713732890663ec5cc55dec1e2
Juan Carlos H <juancarlos_hs@hotmail.com>
2018-03-27 07:33:27 -0600

    Catalogos_P1_P2

---
+++ Caja_Ahorro/ViewController/src/View_M3/M3P01_ClassInsert.java
@@ -0,0 +1,38 @@
+package View_M3;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import oracle.adf.controller.TaskFlowId;
+import oracle.adf.model.BindingContext;
+
+import oracle.binding.BindingContainer;
+import oracle.binding.OperationBinding;
+
+public class M3P01_ClassInsert {
+    private String taskFlowId = "/WEB-INF/Menu3_Task_Insert_P1.xml#Menu3_Task_Insert_P1";
+
+    public M3P01_ClassInsert() {
+    }
+    
+    public BindingContainer getBindings() {
+        return BindingContext.getCurrent().getCurrentBindingsEntry();
+    }
+
+    public String CommitAction() {
+        BindingContainer bindings = getBindings();
+        OperationBinding operationBinding = bindings.getOperationBinding("Commit");
+        Object result = operationBinding.execute();
+        if (!operationBinding.getErrors().isEmpty()) {
+            return null;
+        }
+        FacesContext con = FacesContext.getCurrentInstance();
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "¡Correcto!", "");
+        con.addMessage("", message);
+        return null;
+    }
+
+    public TaskFlowId getDynamicTaskFlowId() {
+        return TaskFlowId.parse(taskFlowId);
+    }
+}
---
+++ Caja_Ahorro/ViewController/src/View_M3/M3P02_Class_Insert.java
@@ -0,0 +1,31 @@
+package View_M3;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import oracle.adf.model.BindingContext;
+
+import oracle.binding.BindingContainer;
+import oracle.binding.OperationBinding;
+
+public class M3P02_Class_Insert {
+    public M3P02_Class_Insert() {
+    }
+
+    public BindingContainer getBindings() {
+        return BindingContext.getCurrent().getCurrentBindingsEntry();
+    }
+
+    public String commitAction() {
+        BindingContainer bindings = getBindings();
+        OperationBinding operationBinding = bindings.getOperationBinding("Commit");
+        Object result = operationBinding.execute();
+        if (!operationBinding.getErrors().isEmpty()) {
+            return null;
+        }
+        FacesContext con = FacesContext.getCurrentInstance();
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "¡Correcto!", "");
+        con.addMessage("", message);
+        return null;
+    }
+}
